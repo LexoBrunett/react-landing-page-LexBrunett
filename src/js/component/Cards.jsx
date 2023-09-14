42097a5713423873cae6c0a0cc7d1eaa00357f99
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-14 00:50:07 +0000

    Final Adjustments

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -6,7 +6,7 @@ export const Cards = () => {
   <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <a href="#" className="btn btn-primary">Find out More!</a>
   </div>
 </div> 
--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const Jumbotron = () => {
 	return (
 		<div className="container p-5 mb-4 bg-light rounded-3">
-      <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">A warm <Welcome></Welcome></h1>
-        <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button className="btn btn-primary btn-lg" type="button">Call to Action!</button>
-      </div>
-    </div>
+			<div className="container-fluid py-5">
+				<h1 className="display-5 fw-bold">A Warm Welcome</h1>
+				<p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button className="btn btn-primary btn-lg" type="button">Call to Action!</button>
+			</div>
+		</div>
 	);
 };
 
