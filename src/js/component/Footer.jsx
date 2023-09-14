2aeae9b0d078a85deeb33ed4c6aec0606c07d59d
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-14 00:46:24 +0000

    Creating the page

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className=" position-absolute bottom-0 col-12 footercontainer text-center bg-dark text-white">
+		<p>Copyright @ LexoBrunett.com 2023</p>
+		<p> Made by LexoBrunett </p>
+
+		</div>
+	);
+};
+
+export default Footer;
