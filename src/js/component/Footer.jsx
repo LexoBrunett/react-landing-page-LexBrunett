867263166406ca76e1846e7878c79a5717eec190
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-14 00:57:46 +0000

    modifying footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+var style = {
+    backgroundColor: "#F8F8F8",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "20px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "60px",
+    width: "100%",
+}
 
-const Footer = () => {
-	return (
-		<div className=" position-absolute bottom-0 col-12 footercontainer text-center bg-dark text-white">
+var phantom = {
+  display: 'block',
+  padding: '20px',
+  height: '60px',
+  width: '100%',
+}
+
+function Footer({ children }) {
+    return (
+        <div>
+            <div style={phantom} />
+            <div style={style}>
+                { children }
 		<p>Copyright @ LexoBrunett.com 2023</p>
 		<p> Made by LexoBrunett </p>
+            </div>
+        </div>
+    )
+}
 
-		</div>
-	);
-};
-
-export default Footer;
+export default Footer
