b1dffed2d597011a4c15154cc723d07db4932705
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-18 22:20:30 +0000

    footer correction

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className=" position-absolute bottom-0 col-12 footercontainer text-center bg-dark text-white">
+		<div className=" bottom-0 col-12 footercontainer text-center bg-dark text-white">
 		<p>Copyright @ LexoBrunett.com 2023</p>
 		<p> Made by LexoBrunett </p>
 		</div>
